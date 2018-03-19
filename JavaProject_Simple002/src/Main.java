506ae10a5dcb5a66e82a7dfb3c5dbae4046a5523
chorras3 <33952622+chorras3@users.noreply.github.com>
2018-03-19 12:14:45 +0100

    Added files dragging from Windows File Explorer

---
+++ JavaProject_Simple002/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	static public void main(String[] args){
+		Main app = new Main();
+		app.test01();
+		
+		System.exit(0);
+	}
+	
+	private void test01(){
+		System.out.println("test01()");
+	}
+}
